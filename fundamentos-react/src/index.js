8ac4952be6c21a12335df6ea90218728d146f6f3
Carlos Eduardo Thompson <carlosethompson@gmail.com>
2020-08-24 15:33:18 +0100

    knowing JSX

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom'
+import React from 'react' // To use JSX
 
-ReactDOM.render(
-  'Hello React!',
+const tag = <strong>Hello React!</strong>;
+
+// Using JSX '<div>'
+ReactDOM.render( 
+  <div> 
+    { tag }
+  </div>,
   document.getElementById('root')
-)
+);
