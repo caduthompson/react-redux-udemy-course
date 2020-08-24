4e645d0606f7f68e180e84d892b4abf7e4452566
Carlos Eduardo Thompson <carlosethompson@gmail.com>
2020-08-24 15:43:06 +0100

    First Component

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,12 +1,15 @@
+import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react' // To use JSX
+
+import Primeiro from './components/basics/Primeiro'
 
 const tag = <strong>Hello React!</strong>;
 
 // Using JSX '<div>'
 ReactDOM.render( 
   <div> 
-    { tag }
+    <Primeiro></Primeiro>
   </div>,
   document.getElementById('root')
 );
